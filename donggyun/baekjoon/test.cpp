32a0db8170b0e753715339022a05ba000e2bea06
dongkeun <ssddgg99@daum.net>
2023-03-17 00:01:14 +0900

    매일 출석체크 용도로만 올리고 있네요...

--- donggyun/baekjoon/test.cpp
+++ donggyun/baekjoon/test.cpp
@@ -1,18 +1,15 @@
 #include <string>
 #include <iostream>
 #include <vector>
+#include <fcntl.h>
 
 using namespace std;
 
 int main()
 {
 	int a, b;
-	while (1)
-	{
-		cin >> a >> b;
-		if (a == 0 && b == 0) break ;
-		if (a > b) cout << "Yes\n";
-		else cout << "No\n";
-	}
+	int rd_size;
+	while (!(cin >> a >> b).eof())
+		cout << a + b << endl;
 	return (0);
 }
